d9ad92ac132adeb7fdf10f3fed25932b423071f5
Guyddo <guyddogl@gmail.com>
2022-05-26 16:53:39 -0300

    Requisito 16

--- script.js
+++ script.js
@@ -2,6 +2,31 @@ const inputLetter = document.getElementById('carta-texto'); // Input com o texto
 const pLetter = document.getElementById('carta-gerada'); // Tag P nde a carta misteriosa será exibida
 const button = document.getElementById('criar-carta'); // Botão Criar Carta misteriosa
 
+// Requisito 16
+function randomClassStyle() {
+  const arrayStyles = ['newspaper', 'magazine1', 'magazine2'];
+  const randomClass = Math.floor(Math.random() * arrayStyles.length);
+  return classStyle = arrayStyles[randomClass];
+}
+
+function randomClassSize() {
+  const arraySizes = ['medium', 'big', 'reallybig'];
+  const randomClass = Math.floor(Math.random() * arraySizes.length);
+  return classSize = arraySizes[randomClass];
+}
+
+function randomClassRotate() {
+  const arrayRotates = ['rotateleft', 'rotateright'];
+  const randomClass = Math.floor(Math.random() * arrayRotates.length);
+  return classSize = arrayRotates[randomClass];
+}
+
+function randomClassSkew() {
+  const arraySkews = ['skewleft', 'skewright'];
+  const randomClass = Math.floor(Math.random() * arraySkews.length);
+  return classSize = arraySkews[randomClass];
+}
+
 // Requisito 3 e 4
 function generateLetter() {
   const inputValue = inputLetter.value;
@@ -14,6 +39,10 @@ function generateLetter() {
   for (let words = 0; words < arrayWords.length; words += 1) {
     const spanWord = document.createElement('span');
     spanWord.innerText = `${arrayWords[words]}`;
+    spanWord.classList.add(randomClassStyle());
+    spanWord.classList.add(randomClassSize());
+    spanWord.classList.add(randomClassRotate());
+    spanWord.classList.add(randomClassSkew());
     pLetter.appendChild(spanWord);
   }
   console.log(arrayWords);
@@ -21,7 +50,7 @@ function generateLetter() {
 
 // Requisito 5
 function emptyInput() {
-  if (inputLetter.value === '' || inputLetter.value === ' ') {
+  if (inputLetter.value === '' || inputLetter.value === ' ') { // <!----> Possível bug quando o input tem vários espaços
     pLetter.innerText = 'Por favor, digite o conteúdo da carta.';
   } else {
     generateLetter();
