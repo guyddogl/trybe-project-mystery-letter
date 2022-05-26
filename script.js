4e3fec8666cfba3bc2ea1241a01045386df3eacf
Guyddo <guyddogl@gmail.com>
2022-05-26 15:13:19 -0300

    Requisitos 3 e 4

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-// Requisito 3
+const inputLetter = document.getElementById('carta-texto'); // Input com o texto do usuário
+const pLetter = document.getElementById('carta-gerada'); // Tag P nde a carta misteriosa será exibida
+const button = document.getElementById('criar-carta'); // Botão Criar Carta misteriosa
+
+// Requisito 3 e 4
 function generateLetter() {
-  console.log('Clicou');
+  const inputValue = inputLetter.value;
+  const arrayWords = [];
+  const splitString = inputValue.split(' ');
+  for (let index = 0; index < splitString.length; index += 1) {
+    arrayWords.push(splitString[index]);
+  }
+  pLetter.innerText = '';
+  for (let words of arrayWords) {
+    let spanWord = document.createElement('span');
+    spanWord.innerText = `${words}`;
+    pLetter.appendChild(spanWord);
+  }
+  // pLetter.innerHTML = inputLetter.value;
+  console.log(arrayWords);
 }
 
 function createLetter() {
-  const button = document.getElementById('criar-carta');
   button.addEventListener('click', generateLetter);
 }
 createLetter();
